9280f90a7fc261f90aadc3b532674837a1801eeb
JennyShih <>
2021-12-16 11:07:20 +0800

    feat : 初步建置 mockup design system

--- src/view/login/Login.tsx
+++ src/view/login/Login.tsx
@@ -10,49 +10,45 @@ const Login = () => {
   const history = useHistory()
 
   return (
-    <>
-      <Row>
-        <Col span={12} className='ad-login-col ad-login-left'></Col>
-        <Col span={12} className='ad-login-col ad-login-right'>
-          <div className='ad-login-container'>
-            <Switch>
-              <Route exact path={'/login'}>
-                <LoginEntry />
-              </Route>
-              <Route exact path={'/login/create'}>
-                <Create />
-              </Route>
-              <Route exact path={'/login/recover/:uuid?'}>
-                <Recover />
-              </Route>
-              <Route exact path={'/login/contact'}>
-                <Contact />
-              </Route>
-              <Route exact path={'/login/terms&privacy'}>
-                <TermsPrivacy />
-              </Route>
-              <Redirect to='/login' />
-            </Switch>
+    <Row className='ad-login-bg'>
+      <Col span={16}>
+        <div className='ad-login-container'>
+          <Switch>
+            <Route exact path={'/login'}>
+              <LoginEntry />
+            </Route>
+            <Route exact path={'/login/create'}>
+              <Create />
+            </Route>
+            <Route exact path={'/login/recover/:uuid?'}>
+              <Recover />
+            </Route>
+            <Route exact path={'/login/contact'}>
+              <Contact />
+            </Route>
+            <Route exact path={'/login/terms&privacy'}>
+              <TermsPrivacy />
+            </Route>
+            <Redirect to='/login' />
+          </Switch>
+        </div>
+      </Col>
+      <Col span={8}>
+        <div className='ad-login-footer'>
+          <div className='ad-btn-group'>
+            <Button type='link' onClick={() => history.push('/login/contact')}>
+              Contact us
+            </Button>
+            <Button
+              type='link'
+              onClick={() => history.push('/login/terms&privacy')}
+            >
+              Terms & policy
+            </Button>
           </div>
-          <div className='ad-login-footer'>
-            <div className='ad-btn-group'>
-              <Button
-                type='link'
-                onClick={() => history.push('/login/contact')}
-              >
-                Contact us
-              </Button>
-              <Button
-                type='link'
-                onClick={() => history.push('/login/terms&privacy')}
-              >
-                Terms & policy
-              </Button>
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </>
+        </div>
+      </Col>
+    </Row>
   )
 }
 export default Login
