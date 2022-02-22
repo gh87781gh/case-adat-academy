22604497536a19c27fc1b226974786cc1ca9bc6e
JennyShih <>
2022-02-22 11:55:44 +0800

    fix : fix login field issue only for Echo

--- src/storage/static.ts
+++ src/storage/static.ts
@@ -1,9 +1,9 @@
 // Globally but updated only once time
 export default class StaticService {
   static apiUrl: string = `${
-    process.env.REACT_APP_ENV === 'DEV'
-      ? process.env.REACT_APP_API_URL
-      : window.location.origin // TODO local
+    process.env.REACT_APP_ENV === 'PROD'
+      ? window.location.origin
+      : process.env.REACT_APP_API_URL // TODO local?
   }${process.env.REACT_APP_API_VERSION}`
 
   static countryCodeOption: any = [
